67651768a532a13858a1572c4505894c4c7b5166
alaa-sufi <alaasufi227@gmail.com>
2022-04-10 00:38:06 +0200

    fix points and packages

--- resources/views/admin/pages/points.blade.php
+++ resources/views/admin/pages/points.blade.php
@@ -464,6 +464,15 @@
 <script src="{{ asset('/assets/js/core/bootstrap.min.js') }}"></script>
 <script src="{{ asset('/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
 <script src="{{ asset('/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+<script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
 <script src="{{ asset('/assets/js/plugins/fullcalendar.min.js') }}"></script>
 <script src="{{ asset('/assets/js/plugins/chartjs.min.js') }}"></script>
 <script src="{{ asset('/assets/js/plugins/choices.min.js') }}"></script>

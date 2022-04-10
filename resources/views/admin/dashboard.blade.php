f4e2569ed4c7d05dfa0a2ca51417d6e2184cc2b2
alaa-sufi <alaasufi227@gmail.com>
2022-04-10 23:02:09 +0200

    fix points and packages

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -66,7 +66,7 @@
     <!--   Core JS Files   -->
 
     @yield('js')
-    <script src="{{ asset('/assets/js/core/popper.min.js') }}"></script>
+    <!-- <script src="{{ asset('/assets/js/core/popper.min.js') }}"></script>
     <script src="{{ asset('/assets/js/core/bootstrap.min.js') }}"></script>
     <script src="{{ asset('/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ asset('/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
@@ -244,8 +244,7 @@
     </script>
     <script src="{{ asset('/assets/js/plugins/choices.min.js') }}"></script>
 
-    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="{{ asset('/assets/js/soft-ui-dashboard.js') }}"></script>
+    <script src="{{ asset('/assets/js/soft-ui-dashboard.js') }}"></script> -->
 </body>
 
 </html>

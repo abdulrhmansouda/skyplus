d031931bca081c6aebd27dd290344277129c0fbe
alaa-sufi <alaasufi227@gmail.com>
2022-04-22 17:14:15 +0200

    setting and report

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -250,14 +250,16 @@
                 <div class="collapse @if (Route::currentRouteName() === 'admin.setting.binding-app.index' || Route::currentRouteName() === 'admin.setting.change-password.index') show @endif" id="setting">
                     <ul class="ul-links">
                         @if (Auth::user()->isSuperAdmin())
-                            <li class="@if (Route::currentRouteName() === 'admin.setting.binding-app.index') active @endif"><a
+                            <li class="mb-1 @if (Route::currentRouteName() === 'admin.setting.binding-app.index') active @endif"><a
                                     href="{{ route('admin.setting.binding-app.index') }}">الاعدادات الربط</a></li>
                         @endif
-                        <li class="@if (Route::currentRouteName() === 'admin.setting.change-password.index') active @endif"><a
+                        <li class="mb-1 @if (Route::currentRouteName() === 'admin.setting.change-password.index') active @endif"><a
                                 href="{{ route('admin.setting.change-password.index') }}">تغير كلمة المرور</a></li>
 
-                        <li class="@if (Route::currentRouteName() === 'admin.setting.social.index') active @endif"><a
+                        <li class="mb-1 @if (Route::currentRouteName() === 'admin.setting.social.index') active @endif"><a
                                 href="{{ route('admin.setting.social.index') }}">معلومات التواصل</a></li>
+                        <li class="mb-1 @if (Route::currentRouteName() === 'admin.setting.social.index') active @endif"><a
+                                href="{{ route('admin.setting.social.index') }}">اعدادات أٌخرى</a></li>
                     </ul>
                     </a>
             </li>
